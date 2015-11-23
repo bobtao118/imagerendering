00d3e2d0d53722ba1135ed46944ada2dc4d4fb25
Wei TingTao <s952865@go.thu.edu.tw>
2015-11-23 09:51:04 -0600

    Initial Commit

---
+++ imagerendering/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  imagerendering
+//
+//  Created by Wei TingTao on 11/23/15.
+//  Copyright © 2015 weitingtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ imagerendering/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  imagerendering
+//
+//  Created by Wei TingTao on 11/23/15.
+//  Copyright © 2015 weitingtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
